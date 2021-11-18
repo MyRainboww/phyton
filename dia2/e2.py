0e3159469529d0afcd7167d813513df345ad322b
MyRainboww <aibarra1725@gmail.com>
2021-11-17 22:40:41 -0600

    ejercicio dia 2 modificado

--- dia2/e2.py
+++ dia2/e2.py
@@ -3,13 +3,17 @@
 year = int(input("Type a year: "))
 is_leap = False
 
-if year % 4 == 0:
+"""if year % 4 == 0:
     if year % 100 == 0:
         if year % 400 == 0:
             is_leap = True
     elif year % 100 != 0:
-        is_leap = True
+        is_leap = True"""
 
+if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+    is_leap = True
+elif year % 4 == 0 and year % 100 != 0:
+    is_leap = True
 
 if is_leap:
     print(f"{year} is leap")
